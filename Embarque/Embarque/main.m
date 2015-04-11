326db0750b2947bd1aee5fcef272a493cce80c13
cleberhenriques <cleberhenriques@gmail.com>
2015-04-11 10:29:49 -0300

    Created project, initialized Podfile

---
+++ Embarque/Embarque/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Embarque
+//
+//  Created by Cleber Henriques on 4/11/15.
+//  Copyright (c) 2015 Cleber Henriques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
