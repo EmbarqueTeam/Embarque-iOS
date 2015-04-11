91392611185efccd3080e6fe7577e65b0323bbcc
cleberhenriques <cleberhenriques@gmail.com>
2015-04-11 14:30:44 -0300

    Created xib and class for CellAirport

---
+++ Embarque/Embarque/View/EMCellAirport.h
@@ -0,0 +1,15 @@
+//
+//  EMCellAirport.h
+//  Embarque
+//
+//  Created by Cleber Henriques on 4/11/15.
+//  Copyright (c) 2015 Cleber Henriques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMCellAirport : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *cover;
+@property (weak, nonatomic) IBOutlet UILabel *name;
+
+@end
