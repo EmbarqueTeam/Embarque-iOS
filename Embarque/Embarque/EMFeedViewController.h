b2442a1677e04122387bf5734efa86f3d839638e
Henrique Morbin <morbin_@hotmail.com>
2015-04-11 19:30:43 -0300

    Feed View Controller

---
+++ Embarque/Embarque/EMFeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  EMFeedViewController.h
+//  Embarque
+//
+//  Created by Henrique Morbin on 11/04/15.
+//  Copyright (c) 2015 Cleber Henriques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMFeedViewController : UIViewController
+
+@end
---
+++ Embarque/Embarque/EMFeedViewController.m
@@ -0,0 +1,25 @@
+//
+//  EMFeedViewController.m
+//  Embarque
+//
+//  Created by Henrique Morbin on 11/04/15.
+//  Copyright (c) 2015 Cleber Henriques. All rights reserved.
+//
+
+#import "EMFeedViewController.h"
+
+@implementation EMFeedViewController
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+#pragma mark - Actions
+- (IBAction)barButtonPlusTouched:(id)sender
+{
+    
+}
+
+@end
