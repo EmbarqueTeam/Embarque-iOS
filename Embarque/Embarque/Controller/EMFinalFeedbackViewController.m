d17869cf8ca03f7223bab5db01f4fc0b844e8855
cleberhenriques <cleberhenriques@gmail.com>
2015-04-12 03:16:53 -0300

    Designing FinalFeedback view, implemente view controller methods

--- Embarque/Embarque/Controller/EMFinalFeedbackViewController.m
+++ Embarque/Embarque/Controller/EMFinalFeedbackViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "EMFinalFeedbackViewController.h"
+#import "EMSessionManager.h"
+#import "EMFeedback.h"
 
 @interface EMFinalFeedbackViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *textCompany;
+@property (weak, nonatomic) IBOutlet UITextField *textFlight;
 
 @end
 
@@ -16,22 +21,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)btnSendTouched:(id)sender {
+    [[[EMSessionManager sharedInstance] feedbackToCreate] setCompany:self.textCompany.text];
+    [[[EMSessionManager sharedInstance] feedbackToCreate] setCompany:self.textFlight.text];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
-*/
+
+- (IBAction)btnSkipTouched:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
