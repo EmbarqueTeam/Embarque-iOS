ecbb3da64ffc07134ac1138e9f92240734cdfefe
Henrique Morbin <morbin_@hotmail.com>
2015-04-12 02:04:27 -0300

    Add Base View Controller

---
+++ Embarque/Embarque/EMBaseViewController.h
@@ -0,0 +1,14 @@
+//
+//  EMBaseViewController.h
+//  Embarque
+//
+//  Created by Henrique Morbin on 12/04/15.
+//  Copyright (c) 2015 Cleber Henriques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <UIColor-ColorWithHexAndAlpha/UIColor+ColorWithHexAndAlpha.h>
+
+@interface EMBaseViewController : UIViewController
+
+@end
---
+++ Embarque/Embarque/EMBaseViewController.m
@@ -0,0 +1,24 @@
+//
+//  EMBaseViewController.m
+//  Embarque
+//
+//  Created by Henrique Morbin on 12/04/15.
+//  Copyright (c) 2015 Cleber Henriques. All rights reserved.
+//
+
+#import "EMBaseViewController.h"
+
+@interface EMBaseViewController ()
+
+@end
+
+@implementation EMBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.view setBackgroundColor:[UIColor colorWithHex:@"EAF9FB"
+                                               andAlpha:1.0f]];
+}
+
+@end
