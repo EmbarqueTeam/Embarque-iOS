2bdc718211ed970c2b4c4d7f611ca194235aa88f
cleberhenriques <cleberhenriques@gmail.com>
2015-04-12 03:14:10 -0300

    Created FinalFeedbackController to collect some more data

---
+++ Embarque/Embarque/Controller/EMFinalFeedbackViewController.m
@@ -0,0 +1,37 @@
+//
+//  EMFinalFeedbackViewController.m
+//  Embarque
+//
+//  Created by Cleber Henriques on 4/12/15.
+//  Copyright (c) 2015 Cleber Henriques. All rights reserved.
+//
+
+#import "EMFinalFeedbackViewController.h"
+
+@interface EMFinalFeedbackViewController ()
+
+@end
+
+@implementation EMFinalFeedbackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
