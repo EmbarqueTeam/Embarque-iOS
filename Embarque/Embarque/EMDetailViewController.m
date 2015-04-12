5ad2b7517a52d4dac9ffd3e69886fc9499da8d2b
cleberhenriques <cleberhenriques@gmail.com>
2015-04-12 05:22:29 -0300

    Instantiating controller from Feedback storyboard

--- Embarque/Embarque/EMDetailViewController.m
+++ Embarque/Embarque/EMDetailViewController.m
@@ -41,6 +41,9 @@
 
 #pragma mark - Actions
 - (IBAction)barButtonPlusTouched:(id)sender {
+    UIStoryboard *feedbackStoryboard = [UIStoryboard storyboardWithName:@"Feedback" bundle:[NSBundle mainBundle]];
+    UINavigationController *navFeedback = [feedbackStoryboard instantiateViewControllerWithIdentifier:@"FeedbackNavigationController"];
+    [self presentViewController:navFeedback animated:YES completion:nil];
 }
 
 #pragma mark - UITableViewDataSource and UITableViewDelegate
