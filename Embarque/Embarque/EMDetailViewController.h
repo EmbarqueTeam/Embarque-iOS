c91ca2af4190b79d47c64da0583508fbbc3241b7
Henrique Morbin <morbin_@hotmail.com>
2015-04-12 01:26:45 -0300

    TabBar Icons

---
+++ Embarque/Embarque/EMDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  EMDetailViewController.h
+//  Embarque
+//
+//  Created by Henrique Morbin on 12/04/15.
+//  Copyright (c) 2015 Cleber Henriques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMDetailViewController : UIViewController
+
+@end
